4e7d885a3a1528e291e6a49241db027b3235e1e7
Eric LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2011-04-25 18:59:18 +0200

    Added script that prepares the files for the PyPI packaging.

---
+++ prepare_for_PyPI.sh
@@ -0,0 +1,7 @@
+# Getting the Python <2.5 version:
+git read-tree -u --prefix=uncertainties-py23/ python-pre-2.5:uncertainties/ && \
+echo "Python 2.3 version imported"
+# Getting the Python 3+ version:
+#git co python-3 uncertainties/
+#mv uncertainties uncertainties-py30
+
