d45c3dd53c26362d2ac21ee74dca3f1b7be556d8
Eric LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2011-04-25 19:03:35 +0200

    Attempt at importing the programs from the python23 branch.

--- prepare_for_PyPI.sh
+++ prepare_for_PyPI.sh
@@ -1,7 +1,16 @@
+# Let's move the original files out of the way:
+mv uncertainties uncertainties-master
+
 # Getting the Python <2.5 version:
-git read-tree -u --prefix=uncertainties-py23/ python-pre-2.5:uncertainties/ && \
+
+# git read-tree -u --prefix=uncertainties-py23/ python-pre-2.5:uncertainties/ && \
+git checkout python2.3 -- uncertainties
+mv uncertainties uncertainties-py23
 echo "Python 2.3 version imported"
+
 # Getting the Python 3+ version:
 #git co python-3 uncertainties/
 #mv uncertainties uncertainties-py30
 
+# I get back the original directory:
+mv uncertainties-master uncertainties
