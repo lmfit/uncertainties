61192c7bdb2a01732bfe6899e286febdc550acb2
Eric LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2011-04-25 19:08:30 +0200

    Better PyPI packaging tool.

--- prepare_for_PyPI.sh
+++ prepare_for_PyPI.sh
@@ -4,6 +4,7 @@ mv uncertainties uncertainties-master
 # Getting the Python <2.5 version:
 
 # git read-tree -u --prefix=uncertainties-py23/ python-pre-2.5:uncertainties/ && \
+rm -rf uncertainties-py23
 git checkout python2.3 -- uncertainties
 mv uncertainties uncertainties-py23
 echo "Python 2.3 version imported"
