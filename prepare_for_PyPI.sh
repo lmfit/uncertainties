6b767b8b0b1b9cc7f3d7ca67c607626a397e01d4
Eric LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2011-04-26 10:23:30 +0200

    Fixed path.

--- prepare_for_PyPI.sh
+++ prepare_for_PyPI.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # This script must be run before packaging (python setup.py sdist upload).
 
 # WARNING: this script erases any uncertainties-py23 and uncertainties-py25
@@ -9,7 +11,7 @@ git commit -a
 ## Getting the Python 2.5 version:
 
 rm -rf uncertainties-py25
-git archive master uncertainties >! /tmp/u.tar
+git archive master uncertainties > /tmp/u.tar
 tar -C /tmp -xf /tmp/u.tar
 mv /tmp/uncertainties uncertainties-py25
 echo "Python 2.5 version imported"
@@ -17,7 +19,7 @@ echo "Python 2.5 version imported"
 ## Getting the Python 2.3 version:
 
 rm -rf uncertainties-py23
-git archive python2.3 uncertainties >! /tmp/u.tar
+git archive python2.3 uncertainties > /tmp/u.tar
 tar -C /tmp -xf /tmp/u.tar
 mv /tmp/uncertainties uncertainties-py23
 echo "Python 2.3 version imported"
