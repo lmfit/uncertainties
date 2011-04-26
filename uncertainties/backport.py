2be94ba8424676aad23d5d5b910ab0861a8fdc65
Eric LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2011-04-26 17:31:12 +0200

    Tests should now run on more recent versions of Python.

--- uncertainties/backport.py
+++ uncertainties/backport.py
@@ -4,8 +4,12 @@ Partial back-port of functions for older versions of Python.
 
 import sys
 
+# Whatever the version of Python, a few backport.* names are defined
+# (this allows programs to rely on backport.* names: tests can be run
+# even from more modern versions of Python).
+
 # For Python < 2.5:
-if sys.version_info[:2] < (2, 5):
+if sys.version_info < (2, 5):
     
     def any(iterable):
         for element in iterable:
@@ -19,11 +23,19 @@ if sys.version_info[:2] < (2, 5):
                 return False
             return True
         
-    if sys.version_info[:2] < (2, 4):
+    if sys.version_info < (2, 4):
         
         def reversed(sequence):
             return sequence[::-1]
 
         from sets import Set as set
 
+    else:
 
+        reversed = reversed
+        set = set
+        
+else:
+
+    any = any
+    all = all
