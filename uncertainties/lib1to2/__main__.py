44e281c03f8ee7019c2eb23b5c4955e1797033b0
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2013-04-16 21:30:03 +0800

    "python -m uncertainties.lib1to2" now works from the parent of uncertainties/. Reference: http://stackoverflow.com/questions/4042905/what-is-main-py.

---
+++ uncertainties/lib1to2/__main__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+'''
+Fixes code like the 2to3 Python utility, but with fixers from the local fixes
+directory.
+
+(c) 2013 by Eric O. LEBIGOT (EOL).
+'''
+
+# Code inspired by the 2to3 Python code.
+
+import lib2to3.main
+import sys
+sys.exit(lib2to3.main.main('uncertainties.lib1to2.fixes'))
