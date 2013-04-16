c6e5cd09fe8c6d9d147f871e4e41410247a985c0
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2013-04-16 11:44:34 +0800

    More robust code.

--- uncertainties/lib1to2/__init__.py
+++ uncertainties/lib1to2/__init__.py
@@ -5,7 +5,10 @@ Fixes code like the 2to3 Python utility, but with fixers from the local fixes
 directory.
 '''
 
-import lib2to3.main
-import sys
 
-sys.exit(lib2to3.main.main('uncertainties.1to2.fixes'))
+# This module can be parsed through the import of fixers:
+if __name__ == '__main__':
+    
+    import lib2to3.main
+    import sys
+    sys.exit(lib2to3.main.main('uncertainties.lib1to2.fixes'))
