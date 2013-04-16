734b65e7713fad2d8ed6408daa286dfeb9f21dab
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2013-04-16 15:14:31 +0800

    Working test version: the path problem is solved.

--- uncertainties/lib1to2/test_1to2.py
+++ uncertainties/lib1to2/test_1to2.py
@@ -6,6 +6,16 @@ Unit tests for the uncertainties.1to2 package.
 (c) 2013 by Eric O. LEBIGOT (EOL).
 '''
 
+#!!!!!!! test
+
+import lib2to3.refactor
+print lib2to3.refactor.get_fixers_from_package('lib2to3.fixes')
+
+import sys
+sys.path.insert(0, '.')
+import uncertainties.lib1to2.fixes
+
+###############################################################################
 # Code inspired by:
 #
 # - lib2to3.tests.test_fixers.py
