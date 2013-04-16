8e4cc89a63e75cc2a987a3fc8520967694afc60f
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2013-04-16 11:22:29 +0800

    More explicit code.

--- uncertainties/1to2/__init__.py
+++ uncertainties/1to2/__init__.py
@@ -5,7 +5,7 @@ Fixes code like the 2to3 Python utility, but with fixers from the local fixes
 directory.
 '''
 
-from lib2to3.main import main
+import lib2to3.main
 import sys
 
-sys.exit(main('uncertainties.1to2.fixes'))
+sys.exit(lib2to3.main.main('uncertainties.1to2.fixes'))
