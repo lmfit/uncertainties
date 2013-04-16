9172e8f19f539a5d84ccd85a25c411233c383354
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2013-04-16 16:53:13 +0800

    Code update skeleton code ready. Code update tests ready.

--- uncertainties/lib1to2/test_1to2.py
+++ uncertainties/lib1to2/test_1to2.py
@@ -16,8 +16,9 @@ import sys
 import os
 import lib2to3.tests.support as support
 
-# The following lib1to2 module must refer to the *local* package (not
-# to any other installed module):
+# The lib1to2 module must refer (through the __import__() used via
+# support.get_refactorer()) to the *local* package (not to any other
+# installed module):
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), os.pardir))
 
 def check_refactor(refactorer, source, expected):
@@ -33,8 +34,59 @@ def check_refactor(refactorer, source, expected):
         refactorer.refactor_string(support.reformat(source), '<string>'))
 
     assert support.reformat(expected) == new
+
+def check_all(fixer, tests):
+    '''
+    Takes a fixer name (module from fixes) and a mapping that maps
+    code using the obsolete syntax into updated code, and checks
+    whether the code is correctly updated.
+    '''    
+
+    refactorer = support.get_refactorer(
+        fixer_pkg='lib1to2', fixers=[fixer])
+
+    for (input_str, out_str) in tests.items():
+        check_refactor(refactorer, input_str, out_str)
     
-def test_fix_name1():
+def test_fix_std_dev():
+    'Tests the transformation of std_dev() into std_dev.'
+
+
+    tests = {
+        'x.std_dev()': 'x.std_dev',
+        'uncertainties.std_dev(x)': 'uncertainties.std_dev(x)',
+        'std_dev(x)': 'std_dev(x)'
+    }
+
+    check_all('std_dev', tests)
     
-    refactorer = support.get_refactorer(fixer_pkg='lib1to2', fixers=['name1'])
-    check_refactor(refactorer, "oldname = 123", "newname = 123")
+def test_ufloat():
+    '''
+    Test of the transformation of: ufloat(tuple,...) and
+    ufloat(string,...) into ufloat(nominal_value, std_dev, tag=...).
+    '''
+    
+    tests = {
+        # Tuples:
+        'ufloat((3, 0.14))': 'ufloat(3, 0.14)',
+        'ufloat((3, 0.14), "pi")': 'ufloat(3, 0.14, "pi")',
+        "ufloat((3, 0.14), 'pi')": "ufloat(3, 0.14, 'pi')",
+        "x = ufloat((3, 0.14), tag='pi')": "x = ufloat(3, 0.14, tag='pi')",
+
+        # Strings:
+        'ufloat("-1.23(3.4)")': 'ufloat_from_str("-1.23(3.4)")',
+        "ufloat('-1.23(3.4)')": "ufloat_from_str('-1.23(3.4)')",
+        'ufloat("-1.23(3.4)", "var")': 'ufloat_from_str("-1.23(3.4)", "var")',
+        'ufloat("-1.23(3.4)", tag="var")':
+            'ufloat_from_str("-1.23(3.4)", tag="var")',
+
+        # Simple expressions that can be transformed:
+        'ufloat((n, s), tag="var")': 'ufloat(n, s, tag="var")',
+
+        # Simple expressions that cannot be transformed:
+        'ufloat(str_repr, tag="var")': 'ufloat(str_repr, tag="var")',
+        'ufloat(*tuple_repr, tag="var")': 'ufloat(*tuple_repr, tag="var")',
+        'ufloat(*t[0, 0])': 'ufloat(*t[0, 0])'
+    }
+
+    check_all('ufloat', tests)
