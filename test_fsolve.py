9f51de9169e1566f31662d318a127f43a6bcd0b2
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2013-04-23 18:54:45 +0800

    Adding a program that fails because of wrap(). To be investigated.

---
+++ test_fsolve.py
@@ -0,0 +1,22 @@
+from scipy.optimize import fsolve
+import uncertainties as u
+
+Fa0 = u.ufloat((5.0, 0.05))
+v0 = u.ufloat((10., 0.1))
+
+V = u.ufloat((66000.0, 100))  # reactor volume L^3
+k = u.ufloat((3.0, 0.2))      # rate constant L/mol/h
+
+def func(Ca):
+    "Mole balance for a CSTR. Solve this equation for func(Ca)=0"
+    Fa = v0 * Ca     # exit molar flow of A
+    ra = -k * Ca**2  # rate of reaction of A L/mol/h
+    return Fa0 - Fa + V * ra
+
+# CA guess that that 90 % is reacted away
+CA_guess = 0.1 * Fa0 / v0
+
+wrapped_fsolve = u.wrap(fsolve)
+CA_sol = wrapped_fsolve(func, CA_guess)
+
+print 'The exit concentration is {0} mol/L'.format(CA_sol)
