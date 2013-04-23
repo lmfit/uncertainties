6e9324e508ae5e492edb2ef22aed8f6b62dd8b5b
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2013-04-23 15:11:59 +0800

    More precise docstring.

--- uncertainties/__init__.py
+++ uncertainties/__init__.py
@@ -607,7 +607,8 @@ def wrap(f, derivatives_args=itertools.repeat(None), derivatives_kwargs={}):
     analytically (with uncertainties-compatible operators and
     functions like +, *, umath.sin(), etc.).
 
-    f must return a scalar (not a list, etc.).
+    f must return a float-like (not a list, etc.), unless when called
+    with no number with uncertainty.
 
     If the wrapped function is called with no argument that has an
     uncertainty, the value of f is returned.
