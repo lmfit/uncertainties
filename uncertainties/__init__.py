f43c36e579b220ade264364d38099234a02a2887
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2013-04-28 22:59:39 +0800

    Bumped version number. This is a bug-fix release (the new tests do not pass with 2.3.5 because of corner cases of pow()), plus a release with support for corner cases of umath.pow() (which now has the same behavior as math.pow()).

--- uncertainties/__init__.py
+++ uncertainties/__init__.py
@@ -240,7 +240,7 @@ import collections
 import inspect
 
 # Numerical version:
-__version_info__ = (2, 3, 5)
+__version_info__ = (2, 3, 6)
 __version__ = '.'.join(map(str, __version_info__))
 
 __author__ = 'Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>'
