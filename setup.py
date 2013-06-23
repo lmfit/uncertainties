9fa8de36ff919b1fafc9a7c9ef5f7d6825d6a8bb
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2013-06-23 13:07:40 +0800

    Added official support for Jython.

--- setup.py
+++ setup.py
@@ -314,6 +314,7 @@ _of_uncertainty
     'Programming Language :: Python :: 3.1',
     'Programming Language :: Python :: 3.2',
     'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: Implementation :: Jython',
     'Topic :: Education',
     'Topic :: Scientific/Engineering',
     'Topic :: Scientific/Engineering :: Mathematics',
