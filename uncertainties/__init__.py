6a0fb721f55b12a7aaee5642c8ddbd56584e3547
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2013-06-25 11:15:44 +0800

    Work in progress: Updating the way the .6f .6e .6g and .6n formats work.

--- uncertainties/__init__.py
+++ uncertainties/__init__.py
@@ -2774,8 +2774,7 @@ def ufloat_fromstr(representation, tag=None):
         12.3e10+/-5e3
         12.3e10±5e3  # Only as a unicode string (Python 2)
         (-3.1415 +/- 0.0001)e+02
-        # Double-exponent values (useful for parsing back the output
-        # of formats like .6g, 10.6g or 10.6n):
+        # Double-exponent values:
         (-3.1415 +/- 1e-4)e+200
         (1e-20 +/- 3)e100
 
