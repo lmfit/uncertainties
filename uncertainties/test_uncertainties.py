beb455f1c43dd7e26fe0839527c80bc181f06908
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2013-07-19 09:12:01 +0800

    Removed obsolete test.

--- uncertainties/test_uncertainties.py
+++ uncertainties/test_uncertainties.py
@@ -1911,7 +1911,7 @@ def test_format():
 
         for (format_spec, result) in representations.iteritems():
 
-            print "FORMATTING", repr(value), "WITH", format_spec  #!!!!! debug
+            # print "FORMATTING", repr(value), "WITH", format_spec
             
             # Jython 2.5.2 does not always represent NaN as nan or NAN
             # in the CPython way: for example, '%.2g' % float('nan')
