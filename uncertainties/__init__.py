1f39fd9e6218401c86334848c95439609e03c285
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2013-11-19 14:24:46 +0100

    Made the NumPy 1.8 mean() fix official.

--- uncertainties/__init__.py
+++ uncertainties/__init__.py
@@ -1693,6 +1693,7 @@ class AffineScalarFunc(object):
     # To save memory in large arrays:
     __slots__ = ('_nominal_value', 'derivatives')
 
+    # Temporary fix for mean() in NumPy 1.8:
     dtype = collections.namedtuple('dtype', 'type')(lambda value: value)
     
     #! The code could be modify in order to accommodate for non-float
