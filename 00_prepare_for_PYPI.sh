37711178661849296de1b214c852795b80910ab1
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2014-04-08 22:21:20 +0800

    Moved the main code to Python 2.6 (instead of Python 2.5, which now shares its code base with Python 2.3-2.4).

--- 00_prepare_for_PYPI.sh
+++ 00_prepare_for_PYPI.sh
@@ -2,27 +2,27 @@
 
 # This script must be run before packaging (python setup.py sdist upload).
 
-# WARNING: this script erases any uncertainties-py23 and uncertainties-py25
+# WARNING: this script erases any uncertainties-py23 and uncertainties-py26
 # found in the current directory.
 
 ## Only committed versions are packaged, to help with debugging published code:
 git checkout release
 git commit -a
 
-# The Python 2.3 version should always be up to date:
+# The Python 2.3-2.5 version should always be up to date:
 git checkout release_python2.3
 git merge release
 
 # Default branch for working on the code:
 git checkout release
 
-## Getting the Python 2.5 version:
+## Getting the Python 2.6+ version:
 
-rm -rf uncertainties-py25 && \
+rm -rf uncertainties-py26 && \
 git archive --output /tmp/u.tar master uncertainties && \
 tar -C /tmp -xf /tmp/u.tar && \
-mv /tmp/uncertainties uncertainties-py25 && \
-echo "Python 2.5+ version imported"
+mv /tmp/uncertainties uncertainties-py26 && \
+echo "Python 2.6+ version imported"
 
 ## Getting the Python 2.3 version:
 
