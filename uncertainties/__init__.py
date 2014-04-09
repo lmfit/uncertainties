ab569a2f2effafbdc3a689c9e0c5867ca203c12a
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2014-04-09 15:54:01 +0800

    Put back docstring part that had been deleted.

--- uncertainties/__init__.py
+++ uncertainties/__init__.py
@@ -1169,6 +1169,8 @@ def robust_align(orig_str, fill_char, align_option, width):
     '''
     Aligns the given string with the given fill character.
 
+    orig_str -- string to be aligned (str or unicode object).
+    
     fill_char -- if empty, space is used.
 
     align_option -- as accepted by format().
