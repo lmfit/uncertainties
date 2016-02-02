28bfc839e921235ff65eb06238761a3f5dc1de41
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2016-02-02 22:31:11 +0100

    Added Python 3.5 compatibility.

--- setup.py
+++ setup.py
@@ -317,6 +317,7 @@ Main changes:
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: Jython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Education',
