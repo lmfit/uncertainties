7e4b3e23a01f501d434cf88c818739b24ba7e1d0
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2018-10-28 22:28:32 +0100

    Bumped version number.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ else:
 # Common options for distutils/setuptools's setup():
 setup_options = dict(
     name='uncertainties',
-    version='3.0.2',
+    version='3.0.3',
     author='Eric O. LEBIGOT (EOL)',
     author_email='eric.lebigot@normalesup.org',
     url='http://uncertainties-python-package.readthedocs.io/',
