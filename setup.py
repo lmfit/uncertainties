06d6312fafcedd734069c6cb16455275025eeb3b
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2018-10-28 23:47:16 +0100

    Added official Python 3.7 compatibility.
    Tested through Appveyor on Windows.

--- setup.py
+++ setup.py
@@ -320,6 +320,7 @@ Main changes:
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: Jython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Education',
