27dadbc0c3ae896aaaa6355bd30767eb75679862
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2020-06-06 16:01:20 +0200

    Added official support for Python 3.8.

--- setup.py
+++ setup.py
@@ -288,6 +288,7 @@ Main changes:
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: Jython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Education',
