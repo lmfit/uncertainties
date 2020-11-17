2d963e923bb4e622d720621255bd206af7043947
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2020-11-17 08:28:24 +0100

    Added official support for Python 3.9.
    Tests pass (on Linux).

--- setup.py
+++ setup.py
@@ -289,6 +289,7 @@ Main changes:
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: Jython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Education',
