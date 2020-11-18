99a2ab31bb65c57d5268fc09062bfadcd11f0053
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2020-11-18 09:07:40 +0100

    Added info on version 3.1.5.

--- setup.py
+++ setup.py
@@ -131,6 +131,8 @@ Version history
 
 Main changes:
 
+- 3.1.5: added a "p" formatting option, that makes sure that there are always
+  parentheses around the … ± … part of printed numbers.
 - 3.1.4: Python 2.7+ is now required.
 - 3.1.2: Fix for NumPy 1.17 and ``unumpy.ulinalg.pinv()``.
 - 3.1: Variables built through a correlation or covariance matrix, and that
