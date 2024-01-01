d78c98b56993e864749b1073c3642ef306c9e43b
Andrew <andrewgsavage@gmail.com>
2024-01-01 07:37:02 +0000

    add uncertainty array

---
+++ uncertainties/test_uncertainty_array.py
@@ -0,0 +1,225 @@
+"""
+This file contains the tests required by pandas for an ExtensionArray and ExtensionType.
+"""
+import warnings
+
+import numpy as np
+import pandas as pd
+import pandas._testing as tm
+import pytest
+from pandas.core import ops
+from pandas.tests.extension import base
+from pandas.tests.extension.conftest import (
+    as_frame,  # noqa: F401
+    as_array,  # noqa: F401
+    as_series,  # noqa: F401
+    fillna_method,  # noqa: F401
+    groupby_apply_op,  # noqa: F401
+    use_numpy,  # noqa: F401
+)
+
+from .uncertainty_array import UncertaintyArray, UncertaintyDtype, ufloat
+from uncertainties import umath
+
+# from .core import ufloat
+
+@pytest.fixture(params=[True, False])
+def box_in_series(request):
+    """Whether to box the data in a Series"""
+    return request.param
+
+
+@pytest.fixture
+def dtype():
+    return UncertaintyDtype()
+
+
+@pytest.fixture
+def data(request):
+    return UncertaintyArray(
+        [ufloat(i, abs(i)/100) for i in np.arange(start=1.0, stop=101.0)]
+    )
+
+
+@pytest.fixture
+def data_missing():
+    return UncertaintyArray(
+        [ufloat(i, abs(i)/100) for i in [np.nan, 1]]
+    )
+
+
+@pytest.fixture
+def data_for_twos():
+    x = [
+        2.0,
+    ] * 100
+    return UncertaintyArray(
+         [ufloat(i, abs(i)/100) for i in x]
+    )
+
+
+@pytest.fixture(params=["data", "data_missing"])
+def all_data(request, data, data_missing):
+    if request.param == "data":
+        return data
+    elif request.param == "data_missing":
+        return data_missing
+
+
+@pytest.fixture
+def data_repeated(data):
+    """Return different versions of data for count times"""
+
+    def gen(count):
+        for _ in range(count):
+            yield data
+
+    yield gen
+
+
+@pytest.fixture(params=[None, lambda x: x])
+def sort_by_key(request):
+    """
+    Simple fixture for testing keys in sorting methods.
+    Tests None (no key) and the identity key.
+    """
+    return request.param
+
+
+@pytest.fixture
+def data_for_sorting():
+    return UncertaintyArray(
+         [ufloat(i, abs(i)/100) for i in [0.3, 10.0, -50.0]]
+    )
+
+
+@pytest.fixture
+def data_missing_for_sorting():
+    return UncertaintyArray(
+         [ufloat(i, abs(i)/100) for i in [4.0, np.nan, -5.0]]
+    )
+
+
+@pytest.fixture
+def na_cmp():
+    """Binary operator for comparing NA values."""
+    return lambda x, y: umath.isnan(x) & umath.isnan(x)
+
+
+@pytest.fixture
+def na_value():
+    return ufloat(np.nan, np.nan)
+
+
+@pytest.fixture
+def data_for_grouping():
+    a = 1.0
+    b = 2.0**32 + 1
+    c = 2.0**32 + 10
+    x = [b, b, np.nan, np.nan, a, a, b, c]
+    return UncertaintyArray(
+         [ufloat(i, abs(i)/100) for i in x]
+    )
+
+# === missing from pandas extension docs about what has to be included in tests ===
+# copied from pandas/pandas/conftest.py
+_all_arithmetic_operators = [
+    "__add__",
+    "__radd__",
+    "__sub__",
+    "__rsub__",
+    "__mul__",
+    "__rmul__",
+    "__floordiv__",
+    "__rfloordiv__",
+    "__truediv__",
+    "__rtruediv__",
+    "__pow__",
+    "__rpow__",
+    "__mod__",
+    "__rmod__",
+]
+
+
+@pytest.fixture(params=_all_arithmetic_operators)
+def all_arithmetic_operators(request):
+    """
+    Fixture for dunder names for common arithmetic operations
+    """
+    return request.param
+
+
+@pytest.fixture(params=["__eq__", "__ne__", "__le__", "__lt__", "__ge__", "__gt__"])
+def all_compare_operators(request):
+    """
+    Fixture for dunder names for common compare operations
+
+    * >=
+    * >
+    * ==
+    * !=
+    * <
+    * <=
+    """
+    return request.param
+
+
+# commented functions aren't implemented in numpy/pandas
+_all_numeric_reductions = [
+    "sum",
+    "max",
+    "min",
+    "mean",
+    # "prod",
+    "std",
+    "var",
+    "median",
+    "sem",
+    "kurt",
+    "skew",
+]
+
+
+@pytest.fixture(params=_all_numeric_reductions)
+def all_numeric_reductions(request):
+    """
+    Fixture for numeric reduction names.
+    """
+    return request.param
+
+
+_all_boolean_reductions = ["all", "any"]
+
+
+@pytest.fixture(params=_all_boolean_reductions)
+def all_boolean_reductions(request):
+    """
+    Fixture for boolean reduction names.
+    """
+    return request.param
+
+
+_all_numeric_accumulations = ["cumsum", "cumprod", "cummin", "cummax"]
+
+
+@pytest.fixture(params=_all_numeric_accumulations)
+def all_numeric_accumulations(request):
+    """
+    Fixture for numeric accumulation names
+    """
+    return request.param
+
+
+@pytest.fixture
+def invalid_scalar(data):
+    """
+    A scalar that *cannot* be held by this ExtensionArray.
+    The default should work for most subclasses, but is not guaranteed.
+    If the array can hold any item (i.e. object dtype), then use pytest.skip.
+    """
+    return object.__new__(object)
+
+from pandas.tests.extension import base
+
+class TestUncertaintyArray(base.ExtensionTests):
+    pass
---
+++ uncertainties/uncertainty_array.py
@@ -0,0 +1,170 @@
+from __future__ import annotations
+
+import numpy
+import pandas
+import pandas.api.extensions
+from pandas.api.types import is_dtype_equal, is_list_like, is_scalar, pandas_dtype
+
+from .core import UFloat, ufloat
+from uncertainties import pandas_backports
+
+import re
+import numpy as np
+from uncertainties import umath
+
+pandas_release = pandas_backports.pandas_release
+
+@pandas.api.extensions.register_extension_dtype
+class UncertaintyDtype(pandas.api.extensions.ExtensionDtype):
+    """
+    Extension dtype for uncertainty objects.
+    """
+
+    na_value = ufloat(np.nan, np.nan)
+    kind = "O"
+    names = None
+    name = "UncertaintyDtype"
+    type = UFloat
+
+    @classmethod
+    def construct_from_string(cls, name: str):
+        if not isinstance(name, str):
+            raise TypeError(
+                f"'construct_from_string' expects a string, got {type(name)}"
+            )
+
+        if name != cls.name:
+            raise TypeError(f"Cannot construct a '{cls.__name__}' from 'another_type'")
+
+        return cls()
+
+    def construct_array_type(self):
+        return UncertaintyArray
+
+
+
+
+class UncertaintyArray(
+    pandas_backports.OpsMixin, pandas_backports.NDArrayBackedExtensionArray
+):
+    dtype = UncertaintyDtype()
+
+    # scalar used to denote NA value inside our self._ndarray, e.g. -1 for
+    # Categorical, iNaT for Period. Outside of object dtype, self.isna() should
+    # be exactly locations in self._ndarray with _internal_fill_value. See:
+    # https://github.com/pandas-dev/pandas/blob/main/pandas/core/arrays/_mixins.py
+    _internal_fill_value = ufloat(np.nan, np.nan)
+
+    def __init__(self, values, dtype=None, copy: bool = False):
+        if not (
+            isinstance(values, numpy.ndarray) and values.dtype == numpy.dtype("object")
+            and all(isinstance(v, UFloat) for v in values)
+        ):
+            values = self.__ndarray(values)
+        elif copy:
+            values = values.copy()
+
+        super().__init__(values=values, dtype=values.dtype)
+
+    @classmethod
+    def __ndarray(cls, scalars):
+        return numpy.array(
+            [ufloat(scalar) if not isinstance(scalar, UFloat) else scalar for scalar in scalars],
+            "object",
+        )
+
+    @classmethod
+    def _from_sequence(cls, scalars, *, dtype=None, copy=False):
+        if dtype is not None:
+            assert dtype.__class__ is cls.dtype.__class__
+        return cls(cls.__ndarray(scalars))
+
+    _from_sequence_of_strings = _from_sequence
+
+    def astype(self, dtype, copy=True):
+        dtype = pandas_dtype(dtype)
+        if is_dtype_equal(dtype, self.dtype):
+            if not copy:
+                return self
+            else:
+                return self.copy()
+
+        return super().astype(dtype, copy=copy)
+
+    def _cmp_method(self, other, op):
+        """Compare array values, for use in OpsMixin."""
+
+        if is_scalar(other) and (
+            pandas.isna(other) or isinstance(other, self.dtype.type)
+        ):
+            other = type(self)([other])
+
+        if type(other) is not type(self):
+            return NotImplemented
+
+        oshape = getattr(other, "shape", None)
+        if oshape != self.shape and oshape != (1,) and self.shape != (1,):
+            raise TypeError(
+                "Can't compare arrays with different shapes", self.shape, oshape
+            )
+        return op(self._ndarray, other._ndarray)
+
+    def _from_factorized(self, unique, original):
+        return self.__class__(unique)
+
+    def isna(self):
+        return np.array([umath.isnan(x) for x in self._ndarray])
+
+    def _validate_scalar(self, value):
+        """
+        Validate and convert a scalar value to datetime64[ns] for storage in
+        backing NumPy array.
+        """
+        return self._ufloat(value)
+
+    def _validate_searchsorted_value(self, value):
+        """
+        Convert a value for use in searching for a value in the backing numpy array.
+
+        TODO: With pandas 2.0, this may be unnecessary. https://github.com/pandas-dev/pandas/pull/45544#issuecomment-1052809232
+        """
+        return self._validate_setitem_value(value)
+
+    def _validate_setitem_value(self, value):
+        """
+        Convert a value for use in setting a value in the backing numpy array.
+        """
+        if is_list_like(value):
+            _ufloat = self._ufloat
+            return [_ufloat(v) for v in value]
+
+        return self._ufloat(value)
+    
+    def _ufloat(self, value):
+        if not isinstance(value, UFloat):
+            return ufloat(value)
+        return value
+
+    # def _box_func(self, x):
+    #     # if pandas.isna(x):
+    #     #     return pandas.NaT
+    #     try:
+    #         return x.astype(_NP_BOX_DTYPE).item().date()
+    #     except AttributeError:
+    #         x = numpy.datetime64(
+    #             x, "ns"
+    #         )  # Integers are stored with nanosecond precision.
+    #         return x.astype(_NP_BOX_DTYPE).item().date()
+
+    # def astype(self, dtype, copy=True):
+    #     stype = str(dtype)
+    #     if stype.startswith("datetime"):
+    #         if stype == "datetime" or stype == "datetime64":
+    #             dtype = self._ndarray.dtype
+    #         return self._ndarray.astype(dtype, copy=copy)
+    #     elif stype.startswith("<M8"):
+    #         if stype == "<M8":
+    #             dtype = self._ndarray.dtype
+    #         return self._ndarray.astype(dtype, copy=copy)
+
+    #     return super().astype(dtype, copy=copy)
