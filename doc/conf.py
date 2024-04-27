7f877f3300b7ff0ddf794f44d8e9d891df2b2bcf
Matthew Newville <newville@cars.uchicago.edu>
2024-04-27 11:38:05 -0500

    playing with themes, add sphinx_copybutton

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,7 @@ import uncertainties
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx_copybutton']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -42,8 +42,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'uncertainties Python package'
-copyright = u'2010–%d, Eric O. LEBIGOT (EOL)' % date.today().year
+project = u'uncertainties'
+copyright = f'2010–{date.today().year}, Eric O. LEBIGOT (EOL)'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -96,8 +96,11 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
+# html_theme = 'sphinxdoc'
 html_theme = 'bizstyle'
-html_theme = 'sphinxdoc'
+# html_theme = 'cloud'
+html_theme = 'python_docs_theme'
+# html_theme = "pydata_sphinx_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -109,7 +112,7 @@ html_theme = 'sphinxdoc'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "uncertainties"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
