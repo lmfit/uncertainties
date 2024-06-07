9fce38d7a824e331bbccd8cf6d87836fd26c32f1
Matt Newville <newville@cars.uchicago.edu>
2024-06-07 14:51:23 -0500

    version set to 3.2.1 (#241)

--- uncertainties/__init__.py
+++ uncertainties/__init__.py
@@ -226,7 +226,7 @@ from .core import *
 from .core import __all__  # For a correct help(uncertainties)
 
 # Numerical version:
-__version_info__ = (3, 2, 0)
+__version_info__ = (3, 2, 1)
 __version__ = '.'.join(map(str, __version_info__))
 
 __author__ = 'Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>'
