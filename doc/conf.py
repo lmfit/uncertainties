f4b0743fc705a73b89d7d6a1589c9b7e7fa9dff8
Justin Gerber <justin.gerber@quantinuum.com>
2024-07-21 08:19:46 -0600

    remove unnecessary import

--- doc/conf.py
+++ doc/conf.py
@@ -18,7 +18,6 @@ import os
 from importlib import metadata
 
 sys.path.insert(0, os.path.abspath(".."))
-import uncertainties
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
