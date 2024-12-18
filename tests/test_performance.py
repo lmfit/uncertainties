f98349e5a868678bb793914cd2c9e98cde88f170
Justin Gerber <justin.gerber@quantinuum.com>
2024-12-18 21:26:15 +0900

    benchmark fixture?

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -2,7 +2,6 @@ from math import log10
 import time
 import timeit
 
-import pytest
 
 from uncertainties import ufloat
 
@@ -39,7 +38,7 @@ def test_complexity():
         assert 0.9 * log10(n / n0) < log10(t / t0) < 1.1 * log10(n / n0)
 
 
-@pytest.mark.parametrize("num", (10, 100, 1000, 10000, 100000))
-@pytest.mark.benchmark
-def test_speed(num):
-    time_ufloat_sum_benchmark(num)
+def test_speed(benchmark):
+    num_list = (10, 100, 1000, 10000, 100000)
+    for num in num_list:
+        benchmark(lambda: time_ufloat_sum_benchmark(num))
