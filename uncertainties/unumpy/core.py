45f2f9513a4b433b5f20eb12ef1e1d07a8b1757f
Justin <justin.gerber48@gmail.com>
2025-05-25 08:00:39 -0600

    do super right

--- uncertainties/unumpy/core.py
+++ uncertainties/unumpy/core.py
@@ -617,7 +617,7 @@ class matrix(numpy.matrix):
             "removed in a future release.",
             FutureWarning,
         )
-        super.__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
     def __rmul__(self, other):
         # ! NumPy's matrix __rmul__ uses an apparently restrictive
