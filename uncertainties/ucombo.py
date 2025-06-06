ceda0d31915443c64c7883fa081ae531a8b51a45
Justin <justin.gerber48@gmail.com>
2025-06-05 23:46:09 -0600

    simplify hash

--- uncertainties/ucombo.py
+++ uncertainties/ucombo.py
@@ -7,18 +7,17 @@ from typing import Optional, Tuple, Union
 
 
 class UAtom:
-    __slots__ = ["uuid", "tag", "_hash"]
+    __slots__ = ["uuid", "tag"]
 
     def __init__(self: UAtom, tag: Optional[str] = None):
         self.tag = tag
         self.uuid: int = random.getrandbits(128)
-        self._hash = hash(self.uuid)
 
     def __eq__(self: UAtom, other: UAtom) -> bool:
         return hash(self) == hash(other)
 
     def __hash__(self: UAtom) -> int:
-        return self._hash
+        return self.uuid
 
     def __lt__(self: UAtom, other: UAtom) -> bool:
         return self.uuid < other.uuid
