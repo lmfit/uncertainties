4a3c3904fc9395c3ce453e73fea3694e34800055
Justin <justin.gerber48@gmail.com>
2025-07-14 00:17:05 -0600

    message formatting

--- uncertainties/core.py
+++ uncertainties/core.py
@@ -446,8 +446,8 @@ class AffineScalarFunc(object):
         This mapping is cached, for subsequent calls.
         """
         warn(
-            f"{self.__class__.___name__}.derivatives() is deprecated. It will be "
-            f"removed in a future release.",
+            f"{self.__class__.___name__}.derivatives() is deprecated. It will "
+            f"be removed in a future release.",
             FutureWarning,
             stacklevel=2,
         )
@@ -476,9 +476,10 @@ class AffineScalarFunc(object):
         math.frexp() returns, for instance).
         """
         warn(
-            f"{self.__class__.___name__}.error_components() is currently an instance "
-            f"method. In a future release it will be become an instance property and "
-            f"will be accessed by {self.__class__.__name__}.error_components (with no "
+            f"{self.__class__.___name__}.error_components() is currently an "
+            f"instance method. This method is deprecated. In a future release it will "
+            f"be replaced with an instance property by the same name. It will be "
+            f"accessed by {self.__class__.__name__}.error_components (with no "
             f"parentheses).",
             FutureWarning,
             stacklevel=2,
